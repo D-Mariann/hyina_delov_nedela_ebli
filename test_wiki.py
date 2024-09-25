0f1012dc5c87d63882cdfff4f97f78ec27d61880
D-Mariann <119792440+D-Mariann@users.noreply.github.com>
2024-09-25 19:14:57 +0300

    Update test_wiki.py

--- test_wiki.py
+++ test_wiki.py
@@ -1,25 +1,9 @@
 import pytest
-import chromedriver_autoinstaller
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-
-#docker run -d -p 4444:4444 --name for_test --shm-size="2g" selenium/standalone-chrome:4.25.0-20240922
-
-
-# @pytest.fixture
-# def driver():
-#     chromedriver_autoinstaller.install()
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('--start-maximized')
-#     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-#     driver = webdriver.Chrome(service=Service(), options=options)
-#     return driver
 
 
 @pytest.fixture
@@ -27,8 +11,6 @@ def driver():
     options = ChromeOptions()
     options.set_capability('se:name', 'test_visit_basic_auth_secured_page (ChromeTests)')
     driver = webdriver.Remote(options=options, command_executor="http://localhost:4444")
-    # driver = webdriver.Chrome()
-    # driver.maximize_window()
     return driver
 
 def test_search_wiki(driver):
